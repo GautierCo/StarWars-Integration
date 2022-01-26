87a7d18801b51c730fd0502465d6ceb8a1716941
GautierCo <gautier.colasse@gmail.com>
2022-01-26 14:47:51 +0100

    Project completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,105 +6,123 @@ const steps = document.querySelectorAll(".step");
 const progressBar = document.querySelector(".naviguation-progress-bar");
 const totalSteps = steps.length;
 let activeStep = 1;
+let previousStep;
 
-const datas = [
-    {
-        name: "Posters",
-        class: "poster-img",
-        activeClass: "poster-active",
-    },
-    {
-        name: "Logos",
-        class: "presentation-logo",
-        activeClass: "logo-active",
-    },
-    {
-        name: "Titles",
-        class: "presentation-title",
-        activeClass: "title-active",
-    },
-    {
-        name: "Descriptions",
-        class: "presentation-description",
-        activeClass: "description-active",
-    },
-];
+const updateDate = () => {
+    const dates = document.querySelectorAll(`.presentation-date`);
+    const nextDates = Array.from(dates[activeStep - 1].children);
 
-const updateDate = (currentStep) => {
-    const currentDates = document.querySelectorAll(".presentation-date")[currentStep - 1]; // []
-    const nextDates = document.querySelectorAll(".presentation-date")[activeStep - 1]; // []
+    nextDates.forEach((nextDate, key) => {
+        const previousDate = dates[previousStep - 1].children[key];
+        const previousNbr = dates[previousStep - 1].children[key].getAttribute("data-date-number");
+        const nextNbr = dates[activeStep - 1].children[key].getAttribute("data-date-number");
+        // const dateWidth = document.querySelector(".presentation-dates").getBoundingClientRect().width;
 
-    Array.from(currentDates.children).forEach((currentDate, key) => {
-        const currentNbr = currentDate.getAttribute("data-date-number");
-        const nextNbr = nextDates.children[key].getAttribute("data-date-number");
-
-        if (currentNbr !== nextNbr) {
-            currentDate.classList.remove("date-active", "next");
-            currentDate.classList.add("back");
-        }
-    });
-
-    Array.from(nextDates.children).forEach((nextDate, key) => {
-        const currentNbr = nextDate.getAttribute("data-date-number");
-        const nextNbr = currentDates.children[key].getAttribute("data-date-number");
-
-        if (currentNbr !== nextNbr) {
-            nextDate.classList.add("date-active", "next");
+        if (previousNbr !== nextNbr) {
+            previousDate.classList.remove("date-active");
+            nextDate.classList.add("date-active");
         }
     });
 };
 
-const changeDatas = (currentStep, classDirection) => {
-    datas.forEach((data) => {
-        const currentPoster = document.querySelectorAll(`.${data.class}`)[currentStep - 1];
-        const nextPoster = document.querySelectorAll(`.${data.class}`)[activeStep - 1];
+const updatePoster = (nextStep) => {
+    const posters = document.querySelectorAll(`.poster-img`);
+    const posterContainerWidth = document.querySelector(".poster-imgs").getBoundingClientRect().width;
 
-        currentPoster.classList.remove(data.activeClass, "next", "back");
-
-        nextPoster.classList.remove("next", "back");
-        nextPoster.classList.add(data.activeClass, classDirection);
+    posters.forEach((poster) => {
+        poster.style.transition = `.255s linear`;
+        poster.style.transform = `translateX(${-nextStep * posterContainerWidth}px)`;
     });
+};
+
+const updateHeader = (nextStep) => {
+    const elements = [
+        {
+            name: "Logos",
+            class: "presentation-logo",
+            activeClass: "logo-active",
+        },
+        {
+            name: "Titles",
+            class: "presentation-title",
+            activeClass: "title-active",
+        },
+        {
+            name: "Descriptions",
+            class: "presentation-description",
+            activeClass: "description-active",
+        },
+    ];
+
+    elements.forEach((element, key) => {
+        const elements = document.querySelectorAll(`.${element.class}`);
+        const elementWidth = document.querySelector(`.${element.class}`).getBoundingClientRect().width;
+        const nextElement = elements[activeStep - 1];
+        const previousElement = elements[previousStep - 1];
+
+        previousElement.classList.remove(element.activeClass);
+        previousElement.style.opacity = 0;
+
+        nextElement.classList.add(element.activeClass);
+        nextElement.style.opacity = 1;
+
+        elements.forEach((element) => {
+            console.log(0.255 + key / 6);
+            element.style.transition = `${0.255 + key / 6}s linear`;
+            element.style.transform = `translateX(${-nextStep * elementWidth - 15 * nextStep}px)`;
+        });
+    });
+};
+
+const updateColorStep = (action) => {
+    if (action === "remove") {
+        steps[activeStep].querySelector("svg > circle").classList.remove("step-circle-active");
+    } else {
+        steps[activeStep].querySelector("svg > circle").classList.add("step-circle-active");
+    }
+};
+
+const updateProgressBar = (operator) => {
+    if (operator === "+") {
+        progressBar.style.width =
+            parseInt(progressBar.style.width.toString()) + parseInt((activeStep + 1 * 100) / (totalSteps - 1)) + "%";
+    } else {
+        progressBar.style.width =
+            parseInt(progressBar.style.width.toString()) - parseInt((activeStep + 1 * 100) / (totalSteps - 1)) + "%";
+    }
 };
 
 // OnClick next
 arrowRight.addEventListener("click", (e) => {
     if (activeStep < totalSteps - 2) {
-        // Change progressbar width
-        progressBar.style.width =
-            parseInt(progressBar.style.width.toString()) + parseInt((activeStep + 1 * 100) / (totalSteps - 1)) + "%";
+        updateColorStep("add");
 
-        // Change color of the current step
-        steps[activeStep].querySelector("svg > circle").classList.add("step-circle-active");
-
-        // Incremente activeStep
+        previousStep = activeStep;
         activeStep++;
 
-        // Change
-        changeDatas(activeStep - 1, "back");
-        updateDate(activeStep - 1);
+        updateDate();
+        updateHeader(activeStep - 1);
+        updatePoster(activeStep - 1);
+        updateProgressBar("+");
 
         // Change arrows
-        if (activeStep === totalSteps) arrowRight.classList.remove("arrow-active");
+        if (activeStep === totalSteps - 2) arrowRight.classList.remove("arrow-active");
         if (activeStep > 1) arrowLeft.classList.add("arrow-active");
     }
 });
 
+//
 // OnClick back
 arrowLeft.addEventListener("click", (e) => {
     if (activeStep > 1) {
-        // Decrement activeStep
+        previousStep = activeStep;
         activeStep--;
 
-        // Change color of the current step
-        steps[activeStep].querySelector("svg > circle").classList.remove("step-circle-active");
-
-        // Change
-        changeDatas(activeStep + 1, "next");
-        updateDate(activeStep + 1);
-
-        // Change progressbar width
-        progressBar.style.width =
-            parseInt(progressBar.style.width.toString()) - parseInt((activeStep + 1 * 100) / (totalSteps - 1)) + "%";
+        updateColorStep("remove");
+        updateDate();
+        updateHeader(activeStep - 1);
+        updatePoster(activeStep - 1);
+        updateProgressBar("-");
 
         // Change arrows
         if (activeStep < totalSteps) arrowRight.classList.add("arrow-active");
